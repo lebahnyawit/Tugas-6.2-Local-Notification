149c25eb140aa307016e6fb8e7d04ae21fe61aa8
lebahnyawit <setangila122@gmail.com>
2026-04-18 17:19:56 +0700

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,14 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-// Tanpa final, langsung sebutkan tipe datanya (FlutterLocalNotificationsPlugin)
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Ini BISA pakai const karena teks '@mipmap/ic_launcher' sudah pasti dari awal
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('@mipmap/ic_launcher');
 
@@ -41,18 +39,14 @@ class NotificationPage extends StatelessWidget {
   const NotificationPage({Key? key}) : super(key: key);
 
   void showNotification() {
-    // KITA TIDAK BISA PAKAI CONST DI SINI.
-    // Sebagai gantinya, kata 'final' dibuang dan diganti tipe data 'DateTime'
     DateTime now = DateTime.now();
 
-    // Kata 'final' dibuang, cukup 'String' saja
     String jam = now.hour.toString().padLeft(2, '0');
     String menit = now.minute.toString().padLeft(2, '0');
     String detik = now.second.toString().padLeft(2, '0');
 
     String pesanWaktu = "Anda menekan tombol pada waktu $jam:$menit:$detik";
 
-    // Ini BISA pakai const karena pengaturan channel ini mutlak/tidak berubah
     const AndroidNotificationDetails androidNotificationDetails =
         AndroidNotificationDetails(
           'channel_tugas_6_2',
@@ -70,7 +64,7 @@ class NotificationPage extends StatelessWidget {
         .show(
           id: 0,
           title: 'Pemberitahuan',
-          body: pesanWaktu, // Memasukkan variabel String tadi
+          body: pesanWaktu,
           notificationDetails: notificationDetails,
         )
         .then((_) {
